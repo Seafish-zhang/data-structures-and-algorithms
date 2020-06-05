775703f1e28b08b44a5df594e7436911aaaaa7e5
“zhanghy <384414328@qq.com>
2020-06-05 14:48:17 +0800

    字符串匹配BM算法

--- src/main/java/algorithms/string/StringMatch.java
+++ src/main/java/algorithms/string/StringMatch.java
@@ -9,10 +9,104 @@ package algorithms.string;
 public class StringMatch {
 
   public static void main(String[] args) {
-    String a = "aax1245aaavaaadaac";
-    String b = "aaav";
+    String a = "aax1245877dsaaaavaaadaac";
+    String b = "aavaa";
     System.out.println(bfMatch(a, b));
     System.out.println(rkMatch(a, b));
+    System.out.println(bmMatch(a, b));
+    System.out.println(bm2Match(a, b));
+  }
+
+  private static int bm2Match(String string, String pattern) {
+    int size = 256;
+    char[] a = string.toCharArray();
+    char[] b = pattern.toCharArray();
+    int n = string.length();
+    int m = pattern.length();
+// a,b表示主串和模式串；n，m表示主串和模式串的长度。
+    int[] bc = generateBC(size, b, m);
+    int[] suffix = new int[m];
+    boolean[] prefix = new boolean[m];
+    generateGS(b, m, suffix, prefix);
+    int i = 0; // j表示主串与模式串匹配的第一个字符
+    while (i <= n - m) {
+      int j;
+      for (j = m - 1; j >= 0; --j) { // 模式串从后往前匹配
+        if (a[i + j] != b[j]) {
+          break; // 坏字符对应模式串中的下标是j
+        }
+      }
+      if (j < 0) {
+        return i; // 匹配成功，返回主串与模式串第一个匹配的字符的位置
+      }
+      int x = j - bc[(int) a[i + j]];
+      int y = 0;
+      if (j < m - 1) { // 如果有好后缀的话
+        y = moveByGS(j, m, suffix, prefix);
+      }
+      i = i + Math.max(x, y);
+    }
+    return -1;
+  }
+
+  // j表示坏字符对应的模式串中的字符下标; m表示模式串长度
+  private static int moveByGS(int j, int m, int[] suffix, boolean[] prefix) {
+    int k = m - 1 - j; // 好后缀长度
+    if (suffix[k] != -1) {
+      return j - suffix[k] + 1;
+    }
+    for (int r = j + 2; r <= m - 1; ++r) {
+      if (prefix[m - r] == true) {
+        return r;
+      }
+    }
+    return m;
+  }
+
+  /**
+   * BM算法，假设只匹配字母，所以d等于26.
+   *
+   * @param string  主串
+   * @param pattern 模式串
+   * @return 开始索引
+   */
+  private static int bmMatch(String string, String pattern) {
+    int size = 256;
+    char[] a = string.toCharArray();
+    char[] b = pattern.toCharArray();
+    int n = string.length();
+    int m = pattern.length();
+    int[] bc = generateBC(size, b, m);
+
+    int i = 0; // i表示主串和模式串对齐的第一个字符.
+    while (i <= n - m) {
+      int j;
+      // 模式串从后往前匹配
+      for (j = m - 1; j >= 0; --j) {
+        // 坏字符对应模式串中的下标是j
+        if (a[i + j] != b[j]) {
+          break;
+        }
+      }
+      if (j < 0) {
+        return i; // 匹配成功，返回主串与模式串第一个匹配的字符的位置
+      }
+      // 这里等同于将模式串往后滑动j-bc[(int)a[i+j]]位
+      i = i + (j - bc[(int) a[i + j]]);
+    }
+    return -1;
+  }
+
+  private static int[] generateBC(int size, char[] b, int m) {
+    int[] bc = new int[size];
+    for (int i = 0; i < size; i++) {
+      bc[i] = -1; //初始化
+    }
+    for (int i = 0; i < m; i++) {
+      int ascii = (int) b[i];
+      bc[ascii] = i;
+    }
+    return bc;
   }
 
   /**
@@ -45,7 +139,7 @@ public class StringMatch {
       h = (h * d) % q;
     }
     //最大需要匹配的次数
-    int frequency = size1 - size2 + 1;
+    int frequency = size1 - size2;
     //字符串开始匹配，对patternCode和strCode开始比较，并更新strCode的值
     for (int i = 0; i < frequency; i++) {
       if (strCode == patternCode) {
@@ -85,6 +179,26 @@ public class StringMatch {
       return i - j;
     } else {
       return -1;
+    }
+  }
+
+  // b表示模式串，m表示长度，suffix，prefix数组事先申请好了
+  private static void generateGS(char[] b, int m, int[] suffix, boolean[] prefix) {
+    for (int i = 0; i < m; ++i) { // 初始化
+      suffix[i] = -1;
+      prefix[i] = false;
+    }
+    for (int i = 0; i < m - 1; ++i) { // b[0, i]
+      int j = i;
+      int k = 0; // 公共后缀子串长度
+      while (j >= 0 && b[j] == b[m - 1 - k]) { // 与b[0, m-1]求公共后缀子串
+        --j;
+        ++k;
+        suffix[k] = j + 1; //j+1表示公共后缀子串在b[0, i]中的起始下标
+      }
+      if (j == -1) {
+        prefix[k] = true; //如果公共后缀子串也是模式串的前缀子串
+      }
     }
   }
 
