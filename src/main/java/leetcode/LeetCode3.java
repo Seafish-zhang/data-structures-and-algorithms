e7162ce818d465a37606bc14c9e95f2d1e65d100
Seafish-zhang <384414328@qq.com>
2020-08-11 14:49:12 +0800

    Z字形变换

--- src/main/java/leetcode/LeetCode3.java
+++ src/main/java/leetcode/LeetCode3.java
@@ -24,7 +24,7 @@ public class LeetCode3 {
 
         int length = ori.length();
         // 算出总共组数
-        int groupCount = 0;
+        int groupCount;
         if (length < rowNum) {
             groupCount = 1;
         } else {
