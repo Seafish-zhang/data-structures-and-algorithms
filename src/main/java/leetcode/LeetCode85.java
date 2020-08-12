3da3f8fc9d18f7a1011dee539629412f2965da28
Seafish-zhang <384414328@qq.com>
2020-08-12 15:38:43 +0800

    柱状图中最大矩形面积

--- src/main/java/leetcode/LeetCode85.java
+++ src/main/java/leetcode/LeetCode85.java
@@ -1,36 +1,122 @@
 package leetcode;
 
-public class LeetCode85 {
+import java.util.Arrays;
 
-    private static int start = -1;
-    private static int end = -1;
+public class LeetCode86 {
+
 
     public static void main(String[] args) {
-        System.out.println(maxArea(new int[]{2, 1, 5, 6, 2, 3}));
-        System.out.println(start + " " + end);
-        System.out.println(maxArea(new int[]{5, 1, 5, 1, 6, 1}));
-        System.out.println(start + " " + end);
+        System.out.println(max(new char[][]{
+                {'1', '0', '1', '0'},
+                {'1', '0', '1', '1'}
+        }));
     }
 
     /**
-     * 柱状图中最大矩形面积.
+     * 二维数组中只包含1的最大矩形面积
      *
-     * @param arr 各个柱子高度数组，每个柱子之间间隔单位为1
-     * @return 最大面积
+     * @param arr 二维数组
+     * @return 最大矩形面积
      */
-    private static int maxArea(int[] arr) {
+    private static int maxArea(char[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
         int maxArea = 0;
-        int current;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                current = (j - i) * Math.min(arr[i], arr[j]);
-                if (current > maxArea) {
-                    maxArea = current;
-                    start = i;
-                    end = j;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] height = new int[n];
+
+        Arrays.fill(left, 0);
+        Arrays.fill(right, n);
+        Arrays.fill(left, 0);
+
+        for (int i = 0; i < m; i++) {
+            int currentLeft = 0;
+            int currentRight = n;
+
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == '1') {
+                    height[j]++;
+                } else {
+                    height[j] = 0;
                 }
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == '1') {
+                    left[j] = Math.max(left[j], currentLeft);
+                } else {
+                    left[j] = 0;
+                    currentLeft = j + 1;
+                }
+            }
+
+            for (int j = n - 1; j >= 0; j--) {
+                if (arr[i][j] == '1') {
+                    right[j] = Math.min(right[j], currentRight);
+                } else {
+                    right[j] = n;
+                    currentRight = j;
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                maxArea = Math.max(maxArea, (right[j] - left[j]) * height[j]);
             }
         }
         return maxArea;
+    }
+
+    private static int max(char[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        int maxArea = 1;
+
+        int[][] area = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            if (arr[i][0] == '1') {
+                area[i][0] = 1;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (arr[0][j] == '1') {
+                area[0][j] = 1;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] == '1') {
+                    area[i][j] = Math.min(area[i - 1][j - 1], Math.max(area[i][j - 1], area[i - 1][j])) + 1;
+                }
+                maxArea = Math.max(area[i][j], maxArea);
+            }
+        }
+        return maxArea;
+    }
+
+    private static int getArea(String[][] arr, int i, int j, int m, int n) {
+        // 超界或者当前值为0，返回 0
+        if (i < 0 || i == m || j < 0 || j == n || "0".equals(arr[i][j])) {
+            return 0;
+        }
+        int wide = i;
+        int hight = j;
+        while (wide < m && hight < n) {
+            if (arr[wide + 1][hight].equals("1")
+                    && arr[wide][hight + 1].equals("1")
+                    && arr[wide + 1][hight + 1].equals("1")) {
+                wide++;
+                hight++;
+            } else if (arr[wide + 1][hight].equals("1")) {
+                wide++;
+            } else if (arr[wide][hight + 1].equals("1")) {
+                hight++;
+            } else {
+                break;
+            }
+        }
+        return (wide - i) * (hight - 1);
     }
 }
