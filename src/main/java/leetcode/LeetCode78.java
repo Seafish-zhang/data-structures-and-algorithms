4a2bba08f46b3a63c305d76bce84160428ffa784
Seafish-zhang <384414328@qq.com>
2020-08-13 09:50:20 +0800

    子集,增加去除重复判断

--- src/main/java/leetcode/LeetCode78.java
+++ src/main/java/leetcode/LeetCode78.java
@@ -1,12 +1,16 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LeetCode78 {
 
+    private static Set<String> exist = new HashSet<>();
+
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3};
+        int[] arr = new int[]{1, 2, 2};
         LeetCode77.printlnDoubleList(subset(arr));
     }
 
@@ -18,13 +22,26 @@ public class LeetCode78 {
     }
 
     private static void match(int start, int[] arr, List<Integer> current, List<List<Integer>> result) {
-        result.add(new ArrayList<>(current));
+        String key = listToString(current);
+        if (!exist.contains(key)) {
+            exist.add(key);
+            result.add(new ArrayList<>(current));
+        }
         for (int i = start; i < arr.length; i++) {
             int size = current.size();
             current.add(arr[i]);
             match(i + 1, arr, current, result);
             current.remove(size);
         }
+    }
+
+    private static String listToString(List<Integer> current) {
+        StringBuilder sb = new StringBuilder();
+        if (current != null && current.size() > 0)
+            for (Integer integer : current) {
+                sb.append(integer);
+            }
+        return sb.toString();
     }
 
 }
