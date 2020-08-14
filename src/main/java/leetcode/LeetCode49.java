c6f9fc5c8e6e6b600adfd8ccf8e1a2b0b2232e27
Seafish-zhang <384414328@qq.com>
2020-08-14 15:21:16 +0800

    字符分组

--- src/main/java/leetcode/LeetCode49.java
+++ src/main/java/leetcode/LeetCode49.java
@@ -11,7 +11,7 @@ public class LeetCode49 {
 
     }
 
-    static void printlnDoubleList(List<List<String>> listList) {
+    public static void printlnDoubleList(List<List<String>> listList) {
         System.out.println("{");
         for (List<String> stringList : listList) {
             StringBuilder sb = new StringBuilder();
@@ -28,6 +28,12 @@ public class LeetCode49 {
         System.out.println("}");
     }
 
+    /**
+     * 按照字符串中字符分组
+     *
+     * @param array 字符串数组
+     * @return 分组后结果
+     */
     private static List<List<String>> group(String[] array) {
         Map<String, List<String>> map = new HashMap<>();
         List<String> value;
