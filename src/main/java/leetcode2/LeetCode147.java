928befe653df3ce70059b0b2bf8e1251233e9b45
Seafish-zhang <384414328@qq.com>
2020-08-20 10:52:32 +0800

    链表排序(插入排序和归并排序)

--- src/main/java/leetcode2/LeetCode147.java
+++ src/main/java/leetcode2/LeetCode147.java
@@ -5,6 +5,14 @@ import structure.link.LinkListTest;
 public class LeetCode147 {
 
     public static void main(String[] args) {
+        LinkListTest.Node node1 = getTestNode();
+        System.out.println(node1.print());
+        System.out.println(insertSort(node1).print());
+        node1 = getTestNode();
+        System.out.println(mergeSort(node1).print());
+    }
+
+    private static LinkListTest.Node getTestNode() {
         LinkListTest.Node node1 = new LinkListTest.Node(7);
         LinkListTest.Node node2 = new LinkListTest.Node(5);
         LinkListTest.Node node3 = new LinkListTest.Node(2);
@@ -14,8 +22,7 @@ public class LeetCode147 {
         node2.next = node3;
         node3.next = node4;
         node4.next = node5;
-        System.out.println(node1.print());
-        System.out.println(sort(node1).print());
+        return node1;
     }
 
     /**
@@ -24,7 +31,7 @@ public class LeetCode147 {
      * @param head 链表头结点
      * @return 排好序的链表
      */
-    private static LinkListTest.Node sort(LinkListTest.Node head) {
+    private static LinkListTest.Node insertSort(LinkListTest.Node head) {
         if (head == null || head.next == null) {
             return head;
         }
@@ -51,5 +58,35 @@ public class LeetCode147 {
             }
         }
         return temp.next;
+    }
+
+    /**
+     * 归并排序链表
+     * <p>要求时间复杂度为O(n log n)</p>
+     *
+     * @param head 头结点
+     * @return 排序后链表
+     */
+    private static LinkListTest.Node mergeSort(LinkListTest.Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // 使用归并排序，时间复杂度为O(n log n）
+        LinkListTest.Node slow = head;
+        LinkListTest.Node fast = head;
+        LinkListTest.Node prev = null;
+        // 使用快慢前进步伐找出中间结点
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // 去除第一段的后缀
+        assert prev != null;
+        prev.next = null;
+
+        LinkListTest.Node front = mergeSort(head);
+        LinkListTest.Node back = mergeSort(slow);
+        return LinkListTest.merge(front, back);
     }
 }
