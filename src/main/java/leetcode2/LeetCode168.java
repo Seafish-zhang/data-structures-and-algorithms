4ed4cc6dd7cec3dc8d1f1dcc174a5eb756ee3013
Seafish-zhang <384414328@qq.com>
2020-08-21 14:31:22 +0800

    excel表列名称

---
+++ src/main/java/leetcode2/LeetCode168.java
@@ -0,0 +1,20 @@
+package leetcode2;
+
+public class LeetCode168 {
+
+    public static void main(String[] args) {
+        System.out.println(excelName(1));
+        System.out.println(excelName(28));
+        System.out.println(excelName(703));
+    }
+
+    private static String excelName(int num) {
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            int i = (num - 1) % 26;
+            sb.append((char) (i + 'A'));
+            num = (num - 1) / 26;
+        }
+        return sb.toString();
+    }
+}
