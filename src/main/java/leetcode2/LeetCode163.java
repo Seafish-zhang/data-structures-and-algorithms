964fb635d61b9eef16ad04a2d9be9637b224b2ce
Seafish-zhang <384414328@qq.com>
2020-08-21 11:29:25 +0800

    最大间距

---
+++ src/main/java/leetcode2/LeetCode163.java
@@ -0,0 +1,30 @@
+package leetcode2;
+
+import java.util.Arrays;
+
+public class LeetCode163 {
+    public static void main(String[] args) {
+        System.out.println(maxInterval(new int[]{1, 6, 9, 3}));
+        System.out.println(maxInterval(new int[]{1, 2, 13, 9, 7}));
+    }
+
+    /**
+     * 最大间距
+     * <p>找出给定的无序数组在排序之后，相邻元素的最大间距</p>
+     * <p>如果数组元素小于2个，则返回 0</p>
+     *
+     * @param arr 无序数组
+     * @return 最大间距
+     */
+    private static int maxInterval(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        Arrays.sort(arr);
+        int max = 0;
+        for (int i = 1; i < arr.length; i++) {
+            max = Math.max(max, arr[i] - arr[i - 1]);
+        }
+        return max;
+    }
+}
