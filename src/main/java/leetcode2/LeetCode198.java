e93bb5d855b5756bd8c91b19a85da3f11caee2f4
Seafish-zhang <384414328@qq.com>
2020-08-25 11:03:00 +0800

    打家劫舍（Ⅰ、Ⅱ)

--- src/main/java/leetcode2/LeetCode198.java
+++ src/main/java/leetcode2/LeetCode198.java
@@ -5,9 +5,17 @@ public class LeetCode198 {
     private static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) {
-        maxMoney(new int[]{1, 2, 3, 1});
+
+        maxMoney(new int[]{2, 3, 2}, false);
         System.out.println(max);
-        maxMoney(new int[]{2, 7, 9, 3, 1});
+        max = Integer.MIN_VALUE;
+        maxMoney(new int[]{1, 2, 3, 1}, false);
+        System.out.println(max);
+        max = Integer.MIN_VALUE;
+        maxMoney(new int[]{2, 3, 2}, true);
+        System.out.println(max);
+        max = Integer.MIN_VALUE;
+        maxMoney(new int[]{1, 2, 3, 1}, true);
         System.out.println(max);
     }
 
@@ -17,19 +25,22 @@ public class LeetCode198 {
      *
      * @param arr 房屋金额
      */
-    private static void maxMoney(int[] arr) {
+    private static void maxMoney(int[] arr, boolean circle) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            steal(arr, i, sum);
+            steal(arr, i, sum, i, circle);
         }
     }
 
-    private static void steal(int[] arr, int start, int sum) {
+    private static void steal(int[] arr, int start, int sum, int base, boolean circle) {
         if (start >= arr.length) {
             max = Math.max(max, sum);
         } else {
             for (int i = start; i < arr.length; i++) {
-                steal(arr, i + 2, sum + arr[i]);
+                if (circle && base == arr.length - 1 - i) {
+                    continue;
+                }
+                steal(arr, i + 2, sum + arr[i], base, circle);
             }
         }
     }
