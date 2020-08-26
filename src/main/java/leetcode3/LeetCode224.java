b3c08f59dc028aba42deb5a2dbe90a31ee141a05
Seafish-zhang <384414328@qq.com>
2020-08-26 11:41:17 +0800

    基本计算器

--- src/main/java/leetcode3/LeetCode224.java
+++ src/main/java/leetcode3/LeetCode224.java
@@ -3,7 +3,7 @@ package leetcode3;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class LeetCode223 {
+public class LeetCode224 {
 
     public static void main(String[] args) {
         System.out.println("1 + 1 = " + calculate("1 + 1"));
