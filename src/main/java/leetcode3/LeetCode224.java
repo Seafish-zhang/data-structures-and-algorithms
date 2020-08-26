ef885af16fb44f173f01e7b1d231d2e6330938cb
Seafish-zhang <384414328@qq.com>
2020-08-26 14:26:18 +0800

    基本计算器（Ⅰ、Ⅱ）

--- src/main/java/leetcode3/LeetCode224.java
+++ src/main/java/leetcode3/LeetCode224.java
@@ -9,12 +9,15 @@ public class LeetCode224 {
         System.out.println("1 + 1 = " + calculate("1 + 1"));
         System.out.println("2-1+2 = " + calculate("2-1+2"));
         System.out.println("(1+(4+5+2)-3)+(6+8) = " + calculate("(1+(4+5+2)-3)+(6+8)"));
+        System.out.println("3+2*2 = " + calculate("3+2*2"));
+        System.out.println("(1+(4+5/2)*3)%(2+5) = " + calculate("(1+(4+5/2)*3)%(2+5)"));
+
     }
 
     /**
      * 基本计算器
      * <p>计算表达式结果值</p>
-     * <p>表达式包含非负整数，左“（”右“）”括号，和加“+”减“-”号</p>
+     * <p>表达式包含非负整数，左“（”右“）”括号，和加“+”减“-”乘“*”除“/”余“%”号</p>
      *
      * @param expression 表达式
      * @return 结果值
@@ -41,18 +44,26 @@ public class LeetCode224 {
                 break;
             } else if (bracketStart == -1) {
 
-                if (expression.charAt(i) == '+' || expression.charAt(i) == '-') {
+                if (expression.charAt(i) == '+' || expression.charAt(i) == '-'
+                        || expression.charAt(i) == '*' || expression.charAt(i) == '/'
+                        || expression.charAt(i) == '%') {
                     int num = Integer.parseInt(expression.substring(numStart, i));
-                    numStart = -1;
                     if (first == -1) {
                         first = num;
                         operator = expression.charAt(i);
                     } else {
-                        second = num;
-                        int current = calculate(first, second, operator);
-                        return calculate(expression.substring(0, operatorStart)
-                                + current + expression.substring(i));
+                        if (!order(operator, expression.charAt(i))) {
+                            first = num;
+                            operatorStart = numStart;
+                            operator = expression.charAt(i);
+                        } else {
+                            second = num;
+                            int current = calculate(first, second, operator);
+                            return calculate(expression.substring(0, operatorStart)
+                                    + current + expression.substring(i));
+                        }
                     }
+                    numStart = -1;
                 } else {
                     if (numStart == -1) {
                         numStart = i;
@@ -77,6 +88,19 @@ public class LeetCode224 {
         } else {
             return result;
         }
+    }
+
+    /**
+     * 是否按照先手顺序来
+     *
+     * @param operator    先操作符
+     * @param newOperator 后操作符
+     * @return 先后返回 true 后先 返回false
+     */
+    private static boolean order(char operator, char newOperator) {
+        if (operator == '*' || operator == '/' || operator == '%') {
+            return true;
+        } else return newOperator != '*' && newOperator != '/' && newOperator != '%';
     }
 
     private static boolean isNumeric(String expression) {
